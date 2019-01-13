046d4825d1116b7321ceca9a939b4f5010ea4c6f
mail <aschuma@users.noreply.github.com>
2019-01-13 22:09:30 +0100

    INTERNAL: simplifications - closures not supported

--- fs/localtime.js
+++ fs/localtime.js
@@ -1,32 +1,32 @@
 load('api_timer.js');
 load('strings.js');
 
-let LocalTime = function () {
+let LocalTime = {
 
-  return {
-    midnight: function () {
-    
-      let struct = {
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      };
-    
-      return struct;
-    },
-    now: function () {
+  midnight: function () {
 
-      let now = Timer.now();
-      let timestamp = Timer.fmt("%H:%M:%S", now);
-      let timestampParts = Strings.split(timestamp, ":");
+    let struct = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
 
-      let struct = {
-        hours: JSON.parse(timestampParts[0]),
-        minutes: JSON.parse(timestampParts[1]),
-        seconds: JSON.parse(timestampParts[2])
-      };
+    return struct;
+  },
 
-      return struct;
-    }
-  };
-}(); 
+  now: function () {
+
+    let now = Timer.now();
+    let timestamp = Timer.fmt("%H:%M:%S", now);
+    let timestampParts = Strings.split(timestamp, ":");
+
+    let struct = {
+      hours: JSON.parse(timestampParts[0]),
+      minutes: JSON.parse(timestampParts[1]),
+      seconds: JSON.parse(timestampParts[2])
+    };
+
+    return struct;
+  }
+};
+
